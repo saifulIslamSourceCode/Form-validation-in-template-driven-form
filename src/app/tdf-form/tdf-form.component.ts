972ca8d28444980c3fa08c23bdf842c4d81654e3
Saiful Islam <76849761+saifulIslamSourceCode@users.noreply.github.com>
2025-05-29 02:46:31 +0600

    find validation in template driven form

--- src/app/tdf-form/tdf-form.component.ts
+++ src/app/tdf-form/tdf-form.component.ts
@@ -1,15 +1,18 @@
-import { CommonModule, NgFor } from '@angular/common';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 import { FormsModule, NgForm } from '@angular/forms';
 import { pipe } from 'rxjs';
+import { User } from '../user';
 
 @Component({
   selector: 'app-tdf-form',
-  imports: [NgFor, FormsModule, CommonModule],
+  imports: [NgFor, FormsModule, CommonModule, NgIf],
   templateUrl: './tdf-form.component.html',
   styleUrl: './tdf-form.component.scss'
 })
 export class TdfFormComponent {
   topics: string[] = ["Angular", "React", "Vue"];
+
+  userModel = new User('', 'rob@test.com', 44444444, '', 'morning', true);
 }
